98db838853352e10bcb00ac3ffec9e2fd095fede
KamranYsupov <kamrankek48@gmail.com>
2025-05-08 21:02:05 +0400

    initial

---
+++ api/app/core/config.py
@@ -0,0 +1,51 @@
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Настройки проекта"""
+
+    project_name: str = Field(title='Название проекта')
+    api_v1_prefix: str = Field(title='Префикс первой версии API', default='/api/v1')
+
+    # region Jaeger
+    service_name: str = Field(
+        title='Название сервиса API',
+        alias='API_SERVICE_NAME',
+        default='fastapi-service',
+    )
+    jaeger_host: str = Field(title='Jaeger хост', default='localhost')
+    jaeger_post: int = Field(title='Jaeger порт', default=6831)
+
+    # endregion
+
+    # region БД
+    db_user: str = Field(title='Пользователь БД')
+    db_password: str = Field(title='Пароль БД')
+    db_host: str = Field(title='Хост БД')
+    db_port: int = Field(title='Порт ДБ', default='5432')
+    db_name: str = Field(title='Название БД')
+    metadata_naming_convention: dict[str, str] = Field(
+        default={
+            'ix': 'ix_%(column_0_label)s',
+            'uq': 'uq_%(table_name)s_%(column_0_N_name)s',
+            'ck': 'ck_%(table_name)s_%(constraint_name)s',
+            'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+            'pk': 'pk_%(table_name)s'
+        })
+
+    sqlite_default_url: str = Field(
+        default='sqlite+aiosqlite:///./db.sqlite3'
+    )
+
+    # endregion
+
+    @property
+    def db_url(self) -> str:
+        return self.sqlite_default_url
+
+    class Config:
+        env_file = '.env'
+
+
+settings = Settings()
---
+++ api/app/main.py
@@ -0,0 +1,46 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+from api.app.api.v1 import routers
+from core.config import settings
+
+
+def create_app() -> FastAPI:
+    fastapi_app = FastAPI(
+        title=settings.project_name,
+        default_response_class=ORJSONResponse,
+    )
+
+    fastapi_app.include_router(routers.api_router, prefix=settings.api_v1_prefix)
+
+
+def set_tracing(fastapi_app: FastAPI):
+    trace.set_tracer_provider(
+        TracerProvider(
+            resource=Resource.create(
+                {SERVICE_NAME: settings.service_name}
+            ))
+    )
+    jaeger_exporter = JaegerExporter(
+        agent_host_name=settings.jaeger_host,
+        agent_port=settings.jaeger_post,
+    )
+    span_processor = BatchSpanProcessor(jaeger_exporter)
+    trace.get_tracer_provider().add_span_processor(span_processor)
+
+    FastAPIInstrumentor.instrument_app(fastapi_app)
+
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    set_tracing(app)
+    uvicorn.run(app='app.main:app', reload=True)
